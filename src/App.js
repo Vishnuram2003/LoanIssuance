fa35896eb11446f33091d237fbb6676d14781d07
Vishnuram2003 <vishnu63820@gmail.com>
2024-04-09 14:49:03 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import ViewLoanComponent from './components/viewloan';
-import EditProfileComponent from './components/editprofile';
-=======
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage'; // Assuming LandingPage is in the correct directory
 import Login from './components/login'; // Adjust the path as necessary
 import AppAppBar from './components/AppAppBar';
 import Register from './components/Register'
->>>>>>> 55cd44d13ce7dacebc12b0946980e379ccc2e750
+import EditProfileComponent from './components/editprofile';
+import ViewLoanComponent from './components/viewloan';
 
 function App() {
   const [mode, setMode] = React.useState('light');
@@ -20,22 +15,18 @@ function App() {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="App">
-        <ViewLoanComponent/>
-        <EditProfileComponent/>
-    </div>
-=======
     <BrowserRouter>
       <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/editprofile" element={<EditProfileComponent />} />
+        <Route path="/viewmyloan" element={<ViewLoanComponent />} />
+
         {/* Define more routes as needed */}
       </Routes>
     </BrowserRouter>
->>>>>>> 55cd44d13ce7dacebc12b0946980e379ccc2e750
   );
 }
 
